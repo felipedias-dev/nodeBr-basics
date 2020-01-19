b30cec00a7aff5fc5977f256fe7455b5042975e0
rafael-lip <felipedias.dev@gmail.com>
2020-01-19 17:27:38 -0300

    Add meuMap prototype

--- lists/for-for_in/2.map.js
+++ lists/for-for_in/2.map.js
@@ -1,20 +1,35 @@
 const service = require('./service');
 
+Array.prototype.meuMap = function(callback) {
+  const novoArrayMapeado = [];
+  for (let indice = 0; indice < this.length; indice++) {
+    const resultado = callback(this[indice], indice);
+    novoArrayMapeado.push(resultado);
+  }
+
+  return novoArrayMapeado;
+};
+
 async function main() {
   try {
     const result = await service.obterPessoas('a');
-    const names = [];
-    console.time('forEach');
-    result.results.forEach(function(item) {
-      names.push(item.name);
-    });
-    console.timeEnd('forEach');
+    // const names = [];
+    // console.time('forEach');
+    // result.results.forEach(function(item) {
+    //   names.push(item.name);
+    // });
+    // console.timeEnd('forEach');
 
-    console.time('map');
-    const names2 = result.results.map(pessoa => pessoa.name);
-    console.timeEnd('map');
+    // console.time('map');
+    // const names2 = result.results.map(pessoa => pessoa.name);
+    // console.timeEnd('map');
+    // console.log(names);
+    // console.log(names2);
+
+    const names = result.results.meuMap(
+      (pessoa, indice) => `[${indice}] ${pessoa.name}`
+    );
     console.log(names);
-    console.log(names2);
   } catch (error) {
     console.error('Deu Ruim:', error);
   }
