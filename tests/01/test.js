22b895fc39a7cbf8bb7010c9ed1060e573458a5a
rafael-lip <felipedias.dev@gmail.com>
2020-02-24 19:07:15 -0300

    Add test with mocha

---
+++ tests/01/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const { obterPessoas } = require('./service');
+
+describe('Star Wars Tests', function() {
+  it('deve buscar o R2D2 com o formato correto', async () => {
+    const expected = [{ nome: 'R2-D2', peso: '96' }];
+    const nameBase = 'r2-d2';
+  });
+});
